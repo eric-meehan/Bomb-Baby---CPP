fdcf9b306ba8b6d84786731eaadffff4b9ba5fad
Eric Meehan <eric.meehan@icloud.com>
2020-11-21 15:32:58 -0500

    Implemented the ability for user input through command line arguments

--- Bomb Baby - CPP/main.cpp
+++ Bomb Baby - CPP/main.cpp
@@ -14,7 +14,35 @@ int Dividend(int, int);
 int ConditionedSubtraction(int * [2]);
 
 int main(int argc, const char * argv[]) {
-    Node Goal = Node(4, 7);
+    // Positions of the desired goal
+    int X;
+    int Y;
+    // The user may specify X and Y values through command line arguments
+    if (argc > 1)
+    {
+        try
+        {
+            // These arguments will need to be cast as integers
+            std::string arg;
+            // Get X from the first argument
+            arg = argv[1];
+            X = std::stoi(arg);
+            // Get Y from the second argument
+            arg = argv[2];
+            Y = std::stoi(arg);
+        }
+        catch (std::invalid_argument)
+        {
+            std::cout << "Invalid argument - using default arguments 4 and 7." << std::endl;
+        }
+    }
+    // Alternatively, the default values of 4 and 7 will be used
+    else
+    {
+        X = 4;
+        Y = 7;
+    }
+    Node Goal = Node(X, Y);
     Node Source = Node(1, 1);
     // Assume the goal is possible
     bool Possible = true;
